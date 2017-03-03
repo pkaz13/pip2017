1e739690489b5e25c7b0df7a5f3d9c770fb78702
Rafał Lebioda <rafallebioda@gmail.com>
2017-03-03 12:46:28 +0100

    Poprawki

--- src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
+++ src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * Created by Rafal Lebioda on 02.03.2017.
@@ -15,6 +16,14 @@ public class WebhookMessenger
     @ResponseBody
     String verify(HttpServletRequest request)
     {
+        List<String> lista= Collections.list(request.getParameterNames());
+        String temp="";
+        for(String str :lista)
+        {
+            temp+=str+" ";
+        }
+        return temp;
+        /*
         String verifyToken = request.getParameter("verify_token");
         String mode = request.getParameter("mode");
         String response=request.getParameter("challenge");
@@ -26,5 +35,6 @@ public class WebhookMessenger
         {
             return "Failed validation. Make sure the validation tokens match.";
         }
+        */
     }
 }
