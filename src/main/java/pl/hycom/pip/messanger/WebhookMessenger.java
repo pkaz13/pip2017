1c4a2afc2423cb9b88dca7334534791baa816d99
Patryk Kacperski <patryk.kacperski141095@gmail.com>
2017-03-04 15:16:36 +0100

    Trying to get response from bot

--- src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
+++ src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
@@ -1,10 +1,10 @@
 package pl.hycom.pip.messanger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -15,33 +15,42 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 public class WebhookMessenger
 {
-    @RequestMapping(value = "/webhook", method = GET)
-    @ResponseBody
-    String verify(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("verifing");
-        String verifyToken = request.getParameter("hub.verify_token");
-        String mode = request.getParameter("hub.mode");
-        String response_msg=request.getParameter("hub.challenge");
-        if(verifyToken==null||mode==null|| response_msg==null) {
-            response.setStatus(403);
-            return "Brak parametru !!!";
-        }
-        if(mode.equals("subscribe") && verifyToken.equals("token")) {
-            response.setStatus(200);
-            return response_msg;
-        }
-        else {
-            response.setStatus(403);
+//    @RequestMapping(value = "/webhook", method = GET)
+//    @ResponseBody
+//    String verify(HttpServletRequest request, HttpServletResponse response) {
+//        System.out.println("verifing");
+//        String verifyToken = request.getParameter("hub.verify_token");
+//        String mode = request.getParameter("hub.mode");
+//        String response_msg=request.getParameter("hub.challenge");
+//        if(verifyToken==null||mode==null|| response_msg==null) {
+//            response.setStatus(403);
+//            return "Brak parametru !!!";
+//        }
+//        if(mode.equals("subscribe") && verifyToken.equals("token")) {
+//            response.setStatus(200);
+//            return response_msg;
+//        }
+//        else {
+//            response.setStatus(403);
+//            return "Failed validation. Make sure the validation tokens match.";
+//        }
+//    }
+
+    @RequestMapping(value = "/webhook", method = GET, produces = MediaType.TEXT_PLAIN)
+    public String verify(@RequestParam("hub.verify_token") final String verifyToken,
+                         @RequestParam("hub.mode") final String mode,
+                         @RequestParam("hub.challenge") final String challenge) {
+        if (verifyToken.equals("token") && mode.equals("subscribe")) {
+            return challenge;
+        } else {
             return "Failed validation. Make sure the validation tokens match.";
         }
     }
 
-    @RequestMapping(value = "/webhook", method = POST)
-    @ResponseBody
-    String respond(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("responding");
-        return "Hello World";
+    @RequestMapping(value = "/webhook", method = POST, consumes = MediaType.APPLICATION_JSON,
+            produces = MediaType.APPLICATION_JSON)
+    public void sendMessage(@RequestBody final String body) {
+        System.out.println("Request Body:");
+        System.out.println(body);
     }
-
-
 }
