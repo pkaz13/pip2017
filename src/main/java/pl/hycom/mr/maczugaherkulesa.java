f88a1e691615b4f5c1ebab583824d87baffb606c
Piotr Bednarek <195560@edu.p.lodz.pl>
2017-03-04 23:39:26 +0100

    maczuga herkulesa - test

--- src/main/java/pl/hycom/mr/maczugaherkulesa.java
+++ src/main/java/pl/hycom/mr/maczugaherkulesa.java
@@ -1,18 +1,18 @@
 package pl.hycom.mr;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by Piotr on 2017-03-03.
- */
 @Controller
 public class maczugaherkulesa {
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "maczuga herkulesa  !";
+        return "Hello"
     }
+
 }
