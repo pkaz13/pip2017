361b3c5ef00415ab68cb5a086093f7326a7b016a
rlebioda <rafallebioda@gmail.com>
2017-03-05 02:20:27 +0100

    Added messenger4j library and try to use it to send message to messenger

--- src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
+++ src/main/java/pl/hycom/pip/messanger/WebhookMessenger.java
@@ -1,5 +1,9 @@
 package pl.hycom.pip.messanger;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.exceptions.MessengerApiException;
+import com.github.messenger4j.exceptions.MessengerIOException;
+import com.github.messenger4j.send.MessengerSendClient;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.*;
 import org.springframework.stereotype.Controller;
@@ -7,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import pl.hycom.model.MessageRequestBody;
 import pl.hycom.model.MessageResponse;
+
+import javax.ws.rs.core.*;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -37,34 +43,80 @@ public class WebhookMessenger
     @RequestMapping(value = "/webhook", method = POST, consumes = MediaType.APPLICATION_JSON,
             produces = MediaType.APPLICATION_JSON)
     public void sendMessage(@RequestBody final MessageRequestBody body) {
-        try {
-            for (MessageRequestBody.Entry entry : body.entry) {
-                for (MessageRequestBody.Messaging messaging : entry.messaging) {
-                    MessageResponse messageResponse = new MessageResponse();
-                    messageResponse.recipient = new MessageResponse.Recipient();
-                    messageResponse.recipient.id = messaging.sender.id;
 
-                    messageResponse.message = new MessageResponse.MessageData();
-
-                    if (messaging.message != null && messaging.message.text != null) {
-                        messageResponse.message.text = "Hello World!";
-
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        String responseString = objectMapper.writeValueAsString(messageResponse);
-                        okhttp3.RequestBody requestBody = okhttp3.RequestBody.
-                                create(okhttp3.MediaType.parse("application/json; charset=utf-8"), responseString);
-                        Request request = new Request.Builder().
-                                url("https://graph.facebook.com/v2.6/me/messages?access_token=" + accessToken).
-                                post(requestBody).build();
-                        OkHttpClient client = new OkHttpClient();
-                        Response response = client.newCall(request).execute();
-                        log.info("Sending response:");
-                        log.info(response.toString());
+        if(StringUtils.equals(body.object,"page"))
+        {
+            for(MessageRequestBody.Entry entry : body.entry)
+            {
+                String pageID=entry.id;
+                String time=entry.time;
+                for(MessageRequestBody.Messaging messaging : entry.messaging)
+                {
+                    if(messaging!=null)
+                    {
+                        receivedMessage(messaging);
+                    }
+                    else
+                    {
+                        log.info("Webhook received unknown event: ");
                     }
                 }
             }
-        } catch (IOException e) {
+        }
+//        try {
+//            for (MessageRequestBody.Entry entry : body.entry) {
+//                for (MessageRequestBody.Messaging messaging : entry.messaging) {
+//                    MessageResponse messageResponse = new MessageResponse();
+//                    messageResponse.recipient = new MessageResponse.Recipient();
+//                    messageResponse.recipient.id = messaging.sender.id;
+//
+//                    messageResponse.message = new MessageResponse.MessageData();
+//
+//                    if (messaging.message != null && messaging.message.text != null) {
+//                        messageResponse.message.text = "Hello World!";
+//
+//                        ObjectMapper objectMapper = new ObjectMapper();
+//                        String responseString = objectMapper.writeValueAsString(messageResponse);
+//                        okhttp3.RequestBody requestBody = okhttp3.RequestBody.
+//                                create(okhttp3.MediaType.parse("application/json; charset=utf-8"), responseString);
+//                        Request request = new Request.Builder().
+//                                url("https://graph.facebook.com/v2.6/me/messages?access_token=" + accessToken).
+//                                post(requestBody).build();
+//                        OkHttpClient client = new OkHttpClient();
+//                        Response response = client.newCall(request).execute();
+//                        log.info("Sending response:");
+//                        log.info(response.toString());
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+
+    private void receivedMessage(MessageRequestBody.Messaging messaging )
+    {
+        if(messaging.message.text!=null) {
+            sendTextMessage(messaging.sender.id, messaging.message.text);
+        }
+    }
+
+    private void sendTextMessage(String id,String message)
+    {
+        MessageResponse messageResponse=new MessageResponse();
+        messageResponse.recipient.id=id;
+        messageResponse.message.text=message;
+        callSendApi(messageResponse);
+    }
+
+    private void callSendApi(MessageResponse messageResponse) {
+        MessengerSendClient sendClient = MessengerPlatform.newSendClientBuilder(accessToken).build();
+        try {
+            sendClient.sendTextMessage(messageResponse.recipient.id, messageResponse.message.text);
+        } catch (MessengerApiException | MessengerIOException e) {
             e.printStackTrace();
         }
+
     }
 }
