33ba047a343bf54ab77682bf8d4f901ece9b5dc4
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2017-03-09 23:55:42 +0100

    czystki, poprawki, aktualizacje wersji, itp

--- src/main/java/pl/hycom/pip/messanger/MessengerWebhook.java
+++ src/main/java/pl/hycom/pip/messanger/MessengerWebhook.java
@@ -1,24 +1,24 @@
 package pl.hycom.pip.messanger;
 
-import com.github.messenger4j.MessengerPlatform;
-import com.github.messenger4j.exceptions.MessengerApiException;
-import com.github.messenger4j.exceptions.MessengerIOException;
-import com.github.messenger4j.exceptions.MessengerVerificationException;
-import com.github.messenger4j.receive.MessengerReceiveClient;
-import com.github.messenger4j.send.MessengerSendClient;
-import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.StringUtils;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import pl.hycom.pip.messanger.config.ConfigService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.ws.rs.core.MediaType;
+import com.github.messenger4j.exceptions.MessengerVerificationException;
+import com.github.messenger4j.receive.MessengerReceiveClient;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * Created by Rafal Lebioda on 02.03.2017.
@@ -27,55 +27,38 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 public class MessengerWebhook {
 
-    @Autowired
-    private ConfigService configService;
-//
-//    @Autowired
-//    private MessengerSendClient sendClient;
-    @Autowired
-    private MessengerReceiveClient receiveClient;
+	@Autowired private MessengerReceiveClient receiveClient;
 
-    @RequestMapping(value = "/webhook", method = GET, produces = MediaType.TEXT_PLAIN)
-    @ResponseBody
-    public ResponseEntity<String> verify(@RequestParam("hub.verify_token") final String verifyToken,
-                                         @RequestParam("hub.mode") final String mode,
-                                         @RequestParam("hub.challenge") final String challenge) {
-        //MessengerIntegrationProperties properties = configService.getMsgIntegrationProperties();
-        if (StringUtils.equals(verifyToken, configService.getVerifyToken()) && StringUtils.equals(mode, "subscribe")) {
-            return ResponseEntity.ok(challenge);
-        } else {
-            log.info("Failed validation. Make sure the validation tokens match.");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
+	@RequestMapping(value = "/webhook", method = GET, produces = MediaType.TEXT_PLAIN)
+	@ResponseBody
+	public ResponseEntity<String> verify(
+			@RequestParam("hub.verify_token") final String verifyToken,
+			@RequestParam("hub.mode") final String mode,
+			@RequestParam("hub.challenge") final String challenge) {
 
-    @RequestMapping(value = "/webhook", method = POST, consumes = MediaType.APPLICATION_JSON,
-            produces = MediaType.APPLICATION_JSON)
-    public ResponseEntity<Void> sendMessage(@RequestBody final String payload,
-                                            @RequestHeader(value = "X-Hub-Signature") String signature) {
-        try {
-            //MessengerIntegrationProperties properties = configService.getMsgIntegrationProperties();
-            log.info("Received message - starting prepare answer ");
-//                receiveClient = MessengerPlatform.newReceiveClientBuilder(configService.getAppSecret(), configService.getVerifyToken())
-//                        .onTextMessageEvent(event -> sendTextMessage(event.getSender().getId(), "Hello World !!! Działa ;)"))
-//                        .build();
-            receiveClient.processCallbackPayload(payload, signature);
-            return ResponseEntity.status(HttpStatus.OK).build();
-        } catch (MessengerVerificationException e) {
-            log.error("Error during token verification {} from msg: {}", payload, e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+		try {
+			return ResponseEntity.ok(receiveClient.verifyWebhook(mode, verifyToken, challenge));
+		} catch (MessengerVerificationException e) {
+			log.warn("Webhook verification failed: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+		}
+	}
 
-//    private void sendTextMessage(String id, String message) {
-//        sendClient = MessengerPlatform.newSendClientBuilder(configService.getPageAccessToken()).build();
-//        try {
-//            log.info("Sending answer");
-//            sendClient.sendTextMessage(id, message);
-//        } catch (MessengerApiException | MessengerIOException e) {
-//            log.error("Error during sending answer " + e.getMessage());
-//        }
-//    }
+	@RequestMapping(value = "/webhook", method = POST, consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<Void> sendMessage(
+			@RequestBody final String payload,
+			@RequestHeader(value = "X-Hub-Signature") String signature) {
 
+		log.info("Received message - starting prepare answer ");
+
+		try {
+			receiveClient.processCallbackPayload(payload, signature);
+			return ResponseEntity.status(HttpStatus.OK).build();
+
+		} catch (MessengerVerificationException e) {
+			log.error("Error during token verification {} from msg: {}", payload, e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
 
 }
--- src/main/java/pl/hycom/pip/messanger/config/MessengerConfiguration.java
+++ src/main/java/pl/hycom/pip/messanger/config/MessengerConfiguration.java
@@ -1,13 +1,14 @@
 package pl.hycom.pip.messanger.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.github.messenger4j.MessengerPlatform;
 import com.github.messenger4j.receive.MessengerReceiveClient;
 import com.github.messenger4j.send.MessengerSendClient;
-import org.jvnet.hk2.config.ConfigSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import pl.hycom.pip.messanger.util.MessengerUtility;
+
+import pl.hycom.pip.messanger.handler.MessengerTextMessageHandler;
 
 /**
  * Created by patry on 07/03/2017.
@@ -16,19 +17,25 @@ import pl.hycom.pip.messanger.util.MessengerUtility;
 @Configuration
 public class MessengerConfiguration {
 
-    @Autowired
-    ConfigService configService;
+	@Value("${messenger.pageAccessToken}") private String pageAccessToken;
+	@Value("${messenger.appSecret}") private String appSecret;
+	@Value("${messenger.verifyToken}") private String verifyToken;
 
-    @Bean
-    MessengerReceiveClient getReceiveClient() {
-        return MessengerPlatform.newReceiveClientBuilder(configService.getAppSecret(), configService.getVerifyToken())
-                .onTextMessageEvent(event -> MessengerUtility.sendTextMessage(event.getSender().getId(), "Hello World!"))
-                .build();
-    }
+	@Bean
+	public MessengerTextMessageHandler messengerTextMessageHandler() {
+		return new MessengerTextMessageHandler(sendClient());
+	}
 
-    @Bean
-    MessengerSendClient getSendClient() {
-        return MessengerPlatform.newSendClientBuilder(configService.getPageAccessToken()).build();
-    }
+	@Bean
+	public MessengerReceiveClient receiveClient() {
+		return MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
+				.onTextMessageEvent(messengerTextMessageHandler())
+				.build();
+	}
+
+	@Bean
+	public MessengerSendClient sendClient() {
+		return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
+	}
 
 }
