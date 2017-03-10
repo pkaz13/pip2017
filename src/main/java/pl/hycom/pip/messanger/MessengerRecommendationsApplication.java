63e346fed390279ce1ff8d721b58c1a9f4180888
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2017-03-10 17:32:48 +0100

    testy jednostkowe

--- src/main/java/pl/hycom/pip/messanger/MessengerRecommendationsApplication.java
+++ src/main/java/pl/hycom/pip/messanger/MessengerRecommendationsApplication.java
@@ -2,10 +2,8 @@ package pl.hycom.pip.messanger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "pl.hycom" })
 public class MessengerRecommendationsApplication {
 
 	public static void main(String[] args) {
