7e4e09cbc31d195e013217fcf23bfb7b9e2eb790
mszalek <marcin.szalek@abastra.com>
2017-03-11 22:57:37 +0100

    Added auth for all /admin/** endpoints

---
+++ src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.hycom.pip.messanger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private static final String ROLE_ADMIN = "ADMIN";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole(ROLE_ADMIN)
+                .and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles(ROLE_ADMIN);
+    }
+
+}
