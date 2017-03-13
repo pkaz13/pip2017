666491b8f456fdfbb5479b54a4840bd57e286bc5
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2017-03-13 20:12:19 +0100

    bootstrap

--- src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
+++ src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
@@ -39,9 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web
-				.ignoring()
-				.antMatchers("/static/**");
+	public void configure(WebSecurity webSecurity) {
+		webSecurity.ignoring().antMatchers("/css/**", "/js/**");
 	}
 }
