a236e1b95bee6ee0f8f526d7881bceb849e048b4
Rafał Lebioda <rafallebioda@gmail.com>
2017-03-14 20:43:50 +0100

    Poprawa wyglądu

---
+++ src/main/resources/static/js/greeting.js
@@ -0,0 +1,9 @@
+/**
+    * Created by Rafal Lebioda on 14.03.2017.
+    */
+
+function addText(text) {
+    var x = document.getElementById("messageInput");
+    var temp = x.value;
+    x.value=temp+text;
+}
