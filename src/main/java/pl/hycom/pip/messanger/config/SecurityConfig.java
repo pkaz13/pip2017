55e6c66ec52f498811297a410068e49d62032db9
mszalek <marcin.szalek@abastra.com>
2017-03-14 10:28:39 +0100

    Added personal application properties

--- src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
+++ src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
@@ -10,15 +10,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     private static final String ROLE_ADMIN = "ADMIN";
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/db-admin/console/**").permitAll()
                 .antMatchers("/admin/**").hasRole(ROLE_ADMIN)
-                .and().httpBasic();
+                .anyRequest().authenticated()
+
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+
+                //TODO: usunąć kiedy zrezygnujemy z consoli do łączenia się z H2
+                .and()
+                .csrf().ignoringAntMatchers("/db-admin/console/**")
+
+                .and()
+                .headers().frameOptions().disable();
     }
 
     @Override
@@ -28,9 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/static/**");
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/css/**", "/js/**", "/webhook");
     }
 }
