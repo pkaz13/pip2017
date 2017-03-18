5f89dd5b32b17fe8569d20577a8888327219d37e
Piotr Bednarek <bednarek.piotr@vp.pl>
2017-03-18 16:26:16 +0100

    próba zwrócenia produktu

---
+++ src/main/java/pl/hycom/pip/messanger/handler/ReturnThreeProductsHandler.java
@@ -0,0 +1,40 @@
+package pl.hycom.pip.messanger.handler;
+
+import com.github.messenger4j.exceptions.MessengerApiException;
+import com.github.messenger4j.exceptions.MessengerIOException;
+import com.github.messenger4j.receive.events.TextMessageEvent;
+import com.github.messenger4j.receive.handlers.TextMessageEventHandler;
+import com.github.messenger4j.send.MessengerSendClient;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import pl.hycom.pip.messanger.service.ProductService;
+
+/**
+ * Created by patry on 09/03/2017.
+ */
+@Log4j2
+@Component
+public class ReturnThreeProductsHandler implements TextMessageEventHandler {
+
+	private MessengerSendClient sendClient;
+	private ProductService productService;
+
+	public ReturnThreeProductsHandler(MessengerSendClient sendClient) {
+		this.sendClient = sendClient;
+	}
+
+	@Override
+	public void handle(TextMessageEvent msg) {
+		sendTextMessage(msg.getSender().getId(), productService.findProductById(1).toString());
+	}
+
+	private void sendTextMessage(String id, String message) {
+		log.info("Sending message[" + message + "] to[" + id + "]");
+
+		try {
+			sendClient.sendTextMessage(id, message);
+		} catch (MessengerApiException | MessengerIOException e) {
+			log.error("Error during sending answer", e);
+		}
+	}
+}
