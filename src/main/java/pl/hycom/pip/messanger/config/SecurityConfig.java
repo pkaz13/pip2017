3af55f09b27089fd44fb7826e851e5c7f78bec9f
mszalek <marcin.szalek@abastra.com>
2017-03-18 12:45:56 +0100

    Removed unused @EnableWebSecurity annotation

--- src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
+++ src/main/java/pl/hycom/pip/messanger/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String ROLE_ADMIN = "ADMIN";
