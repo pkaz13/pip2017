9f87c8da8120b66ddc6e37e49ba391e541aa9b9d
Maciej Olejniczak <elurin16@gmail.com>
2017-03-19 21:50:13 +0100

    ograniczenie dlugosci title i subtitle produktu

--- src/main/java/pl/hycom/pip/messanger/model/Product.java
+++ src/main/java/pl/hycom/pip/messanger/model/Product.java
@@ -2,11 +2,7 @@ package pl.hycom.pip.messanger.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -23,9 +19,11 @@ public class Product implements Serializable {
     private Integer id;
 
     @NotNull
+    @Column(length = 80)
     private String name;
 
     @NotNull
+    @Column(length = 80)
     private String description;
 
     @NotNull
