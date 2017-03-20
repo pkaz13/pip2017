aeebceaa1c37e3226fdf7a0c28b338c09980e9b1
Rafał Lebioda <rafallebioda@gmail.com>
2017-03-20 21:26:59 +0100

    Dodanie danych testowych

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,40 @@ values
 ('lodówka','Chłodziarko-zamrażarka Amica FK3296.4F to model wolnostojący, o wysokości 185 cm.','https://6.allegroimg.com/s1024/0015d6/f9edfa9641ed9f06639d88057506/Chlodziarko-zamrazarka-Amica-FK3296.3F-FK3296.3F'),
 ('lodówka','Chłodziarka Amica FM104.4 to wolnostojące urządzenie z komorą niskich temperatur.','https://8.allegroimg.com/s1024/0042ac/757a038e43adae2a24108ecedd98/Chlodziarka-Amica-FM104.4-Chlodziarka'),
 ('lodówka','Manta CLF050 to podblatowa chłodziarko-zamrażarka wolnostojąca o wysokości 49 cm, w kolorze białym. Posiada 2-letnią gwarancję producenta.','https://3.allegroimg.com/s1024/00d44a/09ecb53b43f3a5f823df04cac473/Chlodziarko-zamrazarka-Manta-CLF050-CLF050');
+
+insert into KEYWORDS (word)
+values
+('TOP3'),
+('TOP2'),
+('TOP1'),
+('Najtańszy'),
+('Najdroższy'),
+('Najwydajniejszy'),
+('Innowacyjy'),
+('Funkcjonalny'),
+('Wielozadaniowy'),
+('Edycja limitowana'),
+('Hit sezonu'),
+('Rabat 10%'),
+('Wyprzedaż'),
+('Gwarancja 5 lat'),
+('Cicha praca');
+
+insert into PRODUCTS_KEYWORDS (PRODUCT_ID,KEYWORDS_ID)
+values
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(3,5),
+(3,6),
+(2,7),
+(5,8),
+(6,9),
+(6,10),
+(7,11),
+(8,12),
+(9,13),
+(10,14),
+(5,15);
+
