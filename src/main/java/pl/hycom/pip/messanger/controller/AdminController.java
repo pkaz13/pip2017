d6bd47d67f7e0a40d4a4cf780f70354cbf6987c6
mszalek <marcin.szalek@abastra.com>
2017-03-25 20:14:03 +0100

    greeting test

--- src/main/java/pl/hycom/pip/messanger/controller/AdminController.java
+++ src/main/java/pl/hycom/pip/messanger/controller/AdminController.java
@@ -1,7 +1,9 @@
 package pl.hycom.pip.messanger.controller;
 
-import java.util.List;
-
+import com.github.messenger4j.exceptions.MessengerApiException;
+import com.github.messenger4j.exceptions.MessengerIOException;
+import com.github.messenger4j.profile.MessengerProfileClient;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +12,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.github.messenger4j.exceptions.MessengerApiException;
-import com.github.messenger4j.exceptions.MessengerIOException;
-import com.github.messenger4j.profile.MessengerProfileClient;
-
-import lombok.extern.log4j.Log4j2;
 import pl.hycom.pip.messanger.model.Greeting;
+import pl.hycom.pip.messanger.model.GreetingList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by piotr on 12.03.2017.
@@ -36,7 +36,7 @@ public class AdminController {
 
     @GetMapping("/admin/greeting")
     public String adminGreeting(Model model) {
-        Greeting greeting = new Greeting();
+        /*Greeting greeting = new Greeting();
 
         try {
             List<com.github.messenger4j.profile.Greeting> greetings = profileClient.getWelcomeMessage().getGreetings();
@@ -46,16 +46,24 @@ public class AdminController {
 
         } catch (MessengerApiException | MessengerIOException e) {
             log.error("Error during getting greeting text from facebook", e);
-        }
+        }*/
+        //model.addAttribute("greeting", new Greeting());
 
-        model.addAttribute("greeting", greeting);
+        List<Greeting> greetings = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            greetings.add(new Greeting());
+            greetings.get(i).setContent("ajfnkssa");
+        }
+        GreetingList greetingList = new GreetingList();
+        greetingList.setGreetings(greetings);
+        model.addAttribute("greetings", greetingList);
         return "greeting";
     }
 
     @PostMapping("/admin/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    public String greetingSubmit(@ModelAttribute Greeting[] greeting) {
         try {
-            profileClient.setupWelcomeMessage(greeting.getContent());
+            profileClient.setupWelcomeMessage(greeting[0].getContent());
             log.info("Greeting text correctly updated");
 
         } catch (MessengerApiException | MessengerIOException e) {
---
+++ src/main/java/pl/hycom/pip/messanger/model/GreetingList.java
@@ -0,0 +1,18 @@
+package pl.hycom.pip.messanger.model;
+
+import java.util.List;
+
+/**
+ * Created by marcinus on 25.03.17.
+ */
+public class GreetingList {
+    private List<Greeting> greetings;
+
+    public List<Greeting> getGreetings() {
+        return greetings;
+    }
+
+    public void setGreetings(List<Greeting> greetings) {
+        this.greetings = greetings;
+    }
+}
