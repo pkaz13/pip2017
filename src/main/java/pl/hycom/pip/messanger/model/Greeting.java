004da32c0d92e4ad7081c2dbd39d129583601ca3
mszalek <marcin.szalek@abastra.com>
2017-04-04 10:47:58 +0200

    Added locale validation

--- src/main/java/pl/hycom/pip/messanger/model/Greeting.java
+++ src/main/java/pl/hycom/pip/messanger/model/Greeting.java
@@ -3,6 +3,9 @@ package pl.hycom.pip.messanger.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
+
 /**
  * Created by rafal on 13.03.2017.
  */
@@ -16,5 +19,18 @@ public class Greeting {
     public Greeting(com.github.messenger4j.profile.Greeting profileGreeting) {
         setText(profileGreeting.getText());
         setLocale(profileGreeting.getLocale());
+    }
+
+    public boolean isValid() {
+        String[] locales = locale.split("_");
+        return locales.length == 2 && isLocaleValid(new Locale(locales[0], locales[1]));
+    }
+
+    private boolean isLocaleValid(Locale locale) {
+        try {
+            return locale.getISO3Language() != null && locale.getISO3Country() != null;
+        } catch (MissingResourceException e) {
+            return false;
+        }
     }
 }
