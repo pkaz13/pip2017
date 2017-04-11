e6eea60a21f7703fa1ebc6c200826e0ffd69a27f
Piotr Kaźmierczak <195611@edu.p.lodz.pl>
2017-04-11 12:14:53 +0200

    Polish errors

--- src/main/java/pl/hycom/pip/messanger/model/Keyword.java
+++ src/main/java/pl/hycom/pip/messanger/model/Keyword.java
@@ -1,13 +1,12 @@
 package pl.hycom.pip.messanger.model;
 
-import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 /**
  * Created by patry on 18/03/2017.
@@ -20,18 +19,16 @@ import lombok.NoArgsConstructor;
 public class Keyword implements Serializable {
 
     private static final long serialVersionUID = -8450849049742048985L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 1, max = 100, message = "{keyword.word.size}")
+    private String word;
 
     public Keyword(String word) {
         super();
         this.word = word;
     }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-
-    @NotNull
-    @Column(unique = true)
-    @Size(min = 1, max= 100)
-    private String word;
 }
