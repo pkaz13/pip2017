7b217e1f14a9af1bedadb734a5964bf1c1f0425b
Piotr Kaźmierczak <pkazmierczak13@gmail.com>
2017-05-23 10:53:58 +0200

    Change .travis.yml

--- src/main/java/pl/hycom/pip/messanger/controller/ResetController.java
+++ src/main/java/pl/hycom/pip/messanger/controller/ResetController.java
@@ -5,8 +5,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.hycom.pip.messanger.service.EmailService;
 
@@ -35,7 +33,7 @@ public class ResetController {
     public String sendEmail(HttpServletRequest request) {
         String mail = request.getParameter("email");
         emailService.sendEmail(emailService.constructEmail(mail,"test", "test"));
-        return "/login";
+        return "redirect:/login";
     }
 
 }
