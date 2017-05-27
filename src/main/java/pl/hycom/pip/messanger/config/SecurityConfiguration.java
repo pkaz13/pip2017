d60fe124d931bbe808f867cd6005ca41ad8bc117
maciejole <elurin16@gmail.com>
2017-05-27 20:16:59 +0200

    Add error message to login page

--- src/main/java/pl/hycom/pip/messanger/config/SecurityConfiguration.java
+++ src/main/java/pl/hycom/pip/messanger/config/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
 
                 .and()
-                .formLogin().loginPage("/login").permitAll()
+                .formLogin().loginPage("/login").failureUrl("/login-error.html").permitAll()
 
                 // TODO: usunąć kiedy zrezygnujemy z consoli do łączenia się z H2
                 .and()
