0af050bb7dd82680bd76495546a24da7684d03de
maciejole <elurin16@gmail.com>
2017-05-28 23:19:37 +0200

    FIX and license

--- src/main/java/pl/hycom/pip/messanger/config/ApplicationStartup.java
+++ src/main/java/pl/hycom/pip/messanger/config/ApplicationStartup.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pl.hycom.pip.messanger.config;
 
 import lombok.extern.slf4j.Slf4j;
@@ -44,12 +60,12 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
 
         User user = new User("admin", "admin", "admin@example.com", "admin", "+48923456783");
         Optional<Role> role = roleService.findRoleByName("ROLE_ADMIN");
-        role.ifPresent(r -> {
-            user.setRoles(Collections.singletonList(r));
+        if (role.isPresent()) {
+            user.setRoles(Collections.singletonList(role.get()));
             userService.addUser(user);
-        });
-
-        log.warn("ApplicationStartup.initializeAdminUser() - Initializing admin failed!!");
+        } else {
+            log.warn("ApplicationStartup.initializeAdminUser() - Initializing admin failed!!");
+        }
     }
 
     private void initializeRole(final String roleName) {
