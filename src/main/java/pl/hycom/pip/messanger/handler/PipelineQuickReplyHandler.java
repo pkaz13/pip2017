723878115139f039380d3e5a78dad1b60545f61e
Marcinus <szalek95@gmail.com>
2017-06-05 21:06:42 +0200

    Pipeline Fix

--- src/main/java/pl/hycom/pip/messanger/handler/PipelineQuickReplyHandler.java
+++ src/main/java/pl/hycom/pip/messanger/handler/PipelineQuickReplyHandler.java
@@ -5,16 +5,17 @@ import com.github.messenger4j.receive.handlers.QuickReplyMessageEventHandler;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.h2.util.StringUtils;
 import pl.hycom.pip.messanger.handler.model.Payload;
 import pl.hycom.pip.messanger.pipeline.PipelineException;
 import pl.hycom.pip.messanger.pipeline.PipelineManager;
 import pl.hycom.pip.messanger.pipeline.PipelineProcessor;
-import pl.hycom.pip.messanger.pipeline.model.Pipeline;
 import pl.hycom.pip.messanger.repository.model.Keyword;
 
 import javax.inject.Inject;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,8 +26,11 @@ import java.util.Map;
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class PipelineQuickReplyHandler implements QuickReplyMessageEventHandler {
 
-    private static final String PIPELINECHAIN_NAME = "processQuickReply";
+    private static final String PIPELINECHAIN_NAME = "processMessage";
     private final PipelineManager pipelineManager;
+
+    private static final String YES_ANSWER = "tak";
+    private static final String NO_ANSWER = "nie";
 
     @Override
     public void handle(QuickReplyMessageEvent event) {
@@ -38,14 +42,20 @@ public class PipelineQuickReplyHandler implements QuickReplyMessageEventHandler 
         String payloadString = event.getQuickReply().getPayload();
         Payload payload = gson.fromJson(payloadString, Payload.class);
 
+        List<Keyword> keywords = payload.getKeywords();
+        List<Keyword> keywordsExcluded = payload.getExcludedKeywords();
+        Keyword keywordAsked = payload.getKeywordToBeAsked();
+
         String answer = event.getText();
+        if (StringUtils.equals(answer, YES_ANSWER)) {
+            keywords.add(keywordAsked);
+        } else {
+            keywordsExcluded.add(keywordAsked);
+        }
 
         params.put(PipelineProcessor.SENDER_ID, event.getSender().getId());
-        params.put(PipelineProcessor.MESSAGE, event.getText());
-        params.put(PipelineProcessor.KEYWORDS_EXCLUDED, payload.getExcludedKeywords());
-        params.put(PipelineProcessor.KEYWORDS, payload.getKeywords());
-        params.put(PipelineProcessor.KEYWORD_TO_BE_ASKED, payload.getKeywordToBeAsked());
-        params.put(PipelineProcessor.ANSWER, answer);
+        params.put(PipelineProcessor.MESSAGE, Arrays.toString(keywords.toArray()));
+        params.put(PipelineProcessor.KEYWORDS_EXCLUDED, keywordsExcluded);
 
         try {
             pipelineManager.runProcess(PIPELINECHAIN_NAME, params);
