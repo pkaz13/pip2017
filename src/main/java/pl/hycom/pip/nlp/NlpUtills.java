75665c0607ceeb4325b22ab5abe35c6e16b61d7b
Piotr Bednarek <bednarek.piotr@vp.pl>
2017-06-09 15:34:51 +0200

    próba usunięcia dtd

--- src/main/java/pl/hycom/pip/nlp/NlpUtills.java
+++ src/main/java/pl/hycom/pip/nlp/NlpUtills.java
@@ -3,10 +3,15 @@ package pl.hycom.pip.nlp;
 import lombok.extern.log4j.Log4j2;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 /**
@@ -19,9 +24,20 @@ public class NlpUtills {
         ArrayList<Result> resultList = new ArrayList<Result>();
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
+
         try {
 
             DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setEntityResolver(new EntityResolver() {
+                @Override
+                public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                    if (    systemId.contains("ccl.dtd")) {
+                        return new InputSource(new StringReader(""));
+                    }
+                    else return null;
+
+                }
+            });
             Document doc = builder.parse(is);
             NodeList orthList = doc.getElementsByTagName("orth");
             NodeList baseList = doc.getElementsByTagName("base");
