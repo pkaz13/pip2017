cd5f7d0c62c4d902af2a43da1cfce0a0ca62d9a2
Hubert Pruszyński <hubert.pruszynski@hycom.pl>
2017-06-09 11:50:37 +0200

    PIP-153 zabezpiecznie rowniez w service

--- src/main/java/pl/hycom/pip/messanger/service/GreetingService.java
+++ src/main/java/pl/hycom/pip/messanger/service/GreetingService.java
@@ -1,26 +1,33 @@
 /*
- *   Copyright 2012-2014 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package pl.hycom.pip.messanger.service;
 
-import com.github.messenger4j.exceptions.MessengerApiException;
-import com.github.messenger4j.exceptions.MessengerIOException;
-import com.github.messenger4j.profile.MessengerProfileClient;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.StringUtils;
@@ -28,27 +35,26 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.github.messenger4j.exceptions.MessengerApiException;
+import com.github.messenger4j.exceptions.MessengerIOException;
+import com.github.messenger4j.profile.MessengerProfileClient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import pl.hycom.pip.messanger.controller.model.Greeting;
 import pl.hycom.pip.messanger.controller.model.GreetingListWrapper;
-
-import javax.inject.Inject;
-import javax.validation.Valid;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.*;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
 @Log4j2
 public class GreetingService implements InitializingBean {
 
-
     private static final String DEFAULT_LOCALE = "default";
-    private static  final String DEFAULT_GREETING = "{{user_full_name}}";
+    private static final String DEFAULT_GREETING = "{{user_full_name}}";
 
     @Autowired
     private MessengerProfileClient profileClient;
-
 
     private Map<String, String> availableLocale = new HashMap<>();
 
@@ -94,7 +100,6 @@ public class GreetingService implements InitializingBean {
         }
     }
 
-
     public void addGreetings(@Valid GreetingListWrapper greetingListWrapper) {
         try {
             profileClient.setupWelcomeMessages(greetingListWrapper.extractGreetings());
@@ -105,7 +110,6 @@ public class GreetingService implements InitializingBean {
 
     }
 
-
     public void sortByLocale(List<com.github.messenger4j.profile.Greeting> greetings) {
         greetings.sort((g1, g2) -> StringUtils.compare(g1.getLocale(), g2.getLocale()));
     }
@@ -114,12 +118,11 @@ public class GreetingService implements InitializingBean {
         return greetings.stream().anyMatch(g -> StringUtils.equals(g.getLocale(), locale));
     }
 
-
     public void addGreeting(@Valid Greeting greeting) throws MessengerApiException, MessengerIOException {
 
         List<com.github.messenger4j.profile.Greeting> greetings = getGreetingsWithDefaultLocale();
-        if (greetings.isEmpty() && !greeting.getLocale().equals(DEFAULT_LOCALE) ) {
-            greetings.add(new com.github.messenger4j.profile.Greeting(DEFAULT_GREETING ,DEFAULT_LOCALE));
+        if (greetings.isEmpty() && !greeting.getLocale().equals(DEFAULT_LOCALE)) {
+            greetings.add(new com.github.messenger4j.profile.Greeting(DEFAULT_GREETING, DEFAULT_LOCALE));
         }
         com.github.messenger4j.profile.Greeting profileGreeting = new com.github.messenger4j.profile.Greeting(greeting.getText(), greeting.getLocale());
         greetings.add(profileGreeting);
@@ -127,8 +130,11 @@ public class GreetingService implements InitializingBean {
         log.info("Greeting text correctly updated");
     }
 
-
     public void removeGreeting(@PathVariable String locale) throws MessengerIOException, MessengerApiException {
+        if (StringUtils.isBlank(locale) || StringUtils.equals(DEFAULT_LOCALE, locale)) {
+            return;
+        }
+
         List<com.github.messenger4j.profile.Greeting> greetings = getGreetingsWithDefaultLocale();
         greetings.removeIf(g -> StringUtils.equals(g.getLocale(), locale));
         profileClient.removeWelcomeMessage();
@@ -136,4 +142,3 @@ public class GreetingService implements InitializingBean {
 
     }
 }
-
